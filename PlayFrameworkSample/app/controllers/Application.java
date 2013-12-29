b2aa85555d4b97b89821755042ad460504968efe
ito masaya <itomasaya@team-lab.com>
2013-12-30 00:59:04 +0900

    簡単なフォーム送信処理を実装 Signed-off-by: ito masaya <itomasaya@team-lab.com>

--- PlayFrameworkSample/app/controllers/Application.java
+++ PlayFrameworkSample/app/controllers/Application.java
@@ -2,13 +2,31 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import play.data.*;
+import static play.data.Form.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	public static class SampleForm {
+		public String message;
+	}
+
+	public static Result index() {
+        return ok(index.render("何か", new Form(SampleForm.class)));
     }
+	
+	public static Result send(){
+		Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+		if(!f.hasErrors()){
+		
+			SampleForm data = f.get();
+			String msg = "you typed: " + data.message;
+			return ok(index.render(msg,f));
+		} else {
+			return badRequest(index.render("ERROR", form(SampleForm.class)));
+		}
+		
+	}
 
 }
